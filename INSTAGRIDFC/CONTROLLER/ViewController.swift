d70c49837da341c6722fb54aac7a78bab58ab562
fabrice CONCHOU <fabrice.conchou@laposte.net>
2019-01-20 09:49:59 +0100

    Begin Prog

--- INSTAGRIDFC/CONTROLLER/ViewController.swift
+++ INSTAGRIDFC/CONTROLLER/ViewController.swift
@@ -16,5 +16,64 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var SquareStackView: UIStackView!
+    @IBOutlet weak var TopStackView: UIStackView!
+    @IBOutlet weak var BottomStackView: UIStackView!
+    
+    @IBOutlet weak var TopButtonLeft: UIButton!
+    @IBAction func TopButtonLeft(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var TopButtonRight: UIButton!
+    @IBAction func TopButtonRight(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var BottomButtonLeft: UIButton!
+    @IBAction func BottomButtonLeft(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var BottomButtonRight: UIButton!
+    @IBAction func BottomButtonRight(_ sender: UIButton) {
+    }
+    
+    @IBAction func Four(_ sender: UIButton) {
+        Four()
+    }
+    
+    @IBAction func OneTop(_ sender: UIButton) {
+        OneTop()
+    }
+    
+    
+    @IBAction func OneBottom(_ sender: UIButton) {
+        OneBottom()
+    }
+    
+    private func OneTop() {
+        TopButtonLeft.isHidden = true
+        TopButtonRight.isHidden = false
+        BottomButtonLeft.isHidden = false
+        BottomButtonRight.isHidden = false
+    }
+    
+    private func OneBottom() {
+        TopButtonLeft.isHidden = false
+        TopButtonRight.isHidden = false
+        BottomButtonLeft.isHidden = true
+        BottomButtonRight.isHidden = false
+    }
+    
+    private func Four() {
+        TopButtonLeft.isHidden = false
+        TopButtonRight.isHidden = false
+        BottomButtonLeft.isHidden = false
+        BottomButtonRight.isHidden = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
 
