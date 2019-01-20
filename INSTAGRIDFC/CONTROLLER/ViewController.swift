21c83a05199f524e5184e764faabdb949f7207d7
fabrice CONCHOU <fabrice.conchou@laposte.net>
2019-01-20 11:10:58 +0100

    With selection

--- INSTAGRIDFC/CONTROLLER/ViewController.swift
+++ INSTAGRIDFC/CONTROLLER/ViewController.swift
@@ -15,26 +15,87 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var photo = UIImage ()
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photo = image
+        }
+        else if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            photo = image
+        }
+        
+       TopButtonLeft.setBackgroundImage(photo, for: .normal)
+        //MyImage.isHidden = false
+        //Bouton.isHidden = true
+        dismiss(animated:true, completion: nil)
+    }
     
-    
+    func Action() {
+        let caméra = UIImagePickerController.isSourceTypeAvailable(.camera)
+        let phototèque = UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        
+        switch (caméra, phototèque) {
+        case (true, true):
+            let Choice = UIAlertController(title:nil, message: nil, preferredStyle: .actionSheet)
+            let actionCamera = UIAlertAction(title: "Caméra", style: .default, handler: { _ in self.Selection(pour: .camera)})
+            let actionPhototeque = UIAlertAction(title: "Phototèque", style: .default, handler: { _ in self.Selection(pour: .photoLibrary)})
+            let actionAnnuler = UIAlertAction(title: "Annuler", style: .cancel)
+            Choice.addAction(actionCamera)
+            Choice.addAction(actionPhototeque)
+            Choice.addAction(actionAnnuler)
+            present(Choice, animated: true)
+        case (true, false) :
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = (self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate)
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+            
+        case (false, true) :
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = (self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate)
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        default :
+            break
+            
+            
+        }
+        
+      
+    }
+    func Selection(pour typeSource: UIImagePickerController.SourceType) {
+        let selecteur = UIImagePickerController()
+        selecteur.sourceType = typeSource
+        selecteur.delegate = (self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate) // as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        present(selecteur, animated: true)
+        
+        
+    }
     @IBOutlet weak var SquareStackView: UIStackView!
     @IBOutlet weak var TopStackView: UIStackView!
     @IBOutlet weak var BottomStackView: UIStackView!
     
     @IBOutlet weak var TopButtonLeft: UIButton!
     @IBAction func TopButtonLeft(_ sender: UIButton) {
+        Action()
     }
     
     @IBOutlet weak var TopButtonRight: UIButton!
     @IBAction func TopButtonRight(_ sender: UIButton) {
+        Action()
     }
     
     @IBOutlet weak var BottomButtonLeft: UIButton!
     @IBAction func BottomButtonLeft(_ sender: UIButton) {
+        Action()
     }
     
     @IBOutlet weak var BottomButtonRight: UIButton!
     @IBAction func BottomButtonRight(_ sender: UIButton) {
+        Action()
     }
     
     @IBAction func Four(_ sender: UIButton) {
@@ -73,6 +134,10 @@ class ViewController: UIViewController {
         //Four.setBackgroundImage(Selected, for: .normal)
     
     }
+    
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
